a755965edf1f553beb9848e5cc82828866a9d8c7
Guy Bourne <guybourne@microsoft.com>
2022-08-25 02:39:37 +0000

    removed comments

--- modules/api-location/app/__init__.py
+++ modules/api-location/app/__init__.py
@@ -5,11 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# comment to trigger
-# comment 2
-# comment 3
-# comment 4
-
 
 def create_app(env=None):
     from app.config import config_by_name
