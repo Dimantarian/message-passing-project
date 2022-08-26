052f938f0a110aa0d165e729e79e60df88db3811
Guy Bourne <guybourne@microsoft.com>
2022-08-26 07:03:26 +0000

    corrections for linter

--- modules/api-location/app/udaconnect/controllers.py
+++ modules/api-location/app/udaconnect/controllers.py
@@ -1,12 +1,10 @@
-from datetime import datetime
-
 from app.udaconnect.models import Location
 from app.udaconnect.schemas import LocationSchema
 from app.udaconnect.services import LocationService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
-from typing import Optional, List
+
 
 DATE_FORMAT = "%Y-%m-%d"
 
