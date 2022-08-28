338d628e1fc8ec79e524a583db76801c136a52e8
Guy Bourne <guybourne@microsoft.com>
2022-08-28 05:27:01 +0000

    updated data shell script to conform to namespace

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -4,6 +4,7 @@
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
 
+kubectl config set-context --current --namespace=udaconnect
 
 cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
